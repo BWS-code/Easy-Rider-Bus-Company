fbcaadbf93d16a3cb73a378882b540ccd24a113d
Sergey Shirnin <66397172+BWS-code@users.noreply.github.com>
2022-02-21 20:58:14 +1100

    Stage 2

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import json
+import re
 from collections import defaultdict
-
 
 data = json.loads(input())
 err_dict = defaultdict(int)
 
 check_list = {
-    'bus_id':       [int, 'required'],
-    'stop_id':      [int, 'required'],
-    'stop_name':    [str, 'required'],
-    'next_stop':    [int, 'required'],
-    'stop_type':    [str, '__char__'],
-    'a_time':       [str, 'required']
+    'bus_id': {
+        'data': int,
+        'mandatory': True,
+        'format': None
+    },
+    'stop_id': {
+        'data': int,
+        'mandatory': True,
+        'format': None
+    },
+    'stop_name': {
+        'data': str,
+        'mandatory': True,
+        'format': '^[A-Z]\w+\s.*?(Road|Boulevard|Street|Avenue)$'
+    },
+    'next_stop': {
+        'data': int,
+        'mandatory': True,
+        'format': None
+    },
+    'stop_type': {
+        'data': str,
+        'mandatory': False,
+        'format': '^[SOF]?$'
+    },
+    'a_time': {
+        'data': str,
+        'mandatory': True,
+        'format': '^(0\d|1\d|2[0-3]):[0-5]\d$'
+    }
 }
 
 for element in data:
     for k, v in element.items():
-        data_type = check_list[k][0]
-        required_field = check_list[k][1] == 'required'
-        char_type = check_list[k][1] == '__char__'
-     
-        if type(v) != data_type or \
-                required_field and data_type == str and not v or \
-                char_type and len(v) > 1:
+        if check_list[k]['format'] and \
+        re.match(check_list[k]['format'], str(v)) is None:
             err_dict[k] += 1
 
 errors = sum(v for v in err_dict.values())
 
-print(f'Type and required field validation: {errors} error{"s"[:errors^1]}')
+print(f'Type and required field validation: {errors} error{"s"[:errors ^ 1]}')
 for check in check_list:
-    print(f'{check}: {err_dict[check]}')
+    if check_list[check]['format']:
+        print(f'{check}: {err_dict[check]}')
